ae3e9d9dc31cdd419c2fa3919da0dd24d26582a2
Izana7822 <70507501+Izana7822@users.noreply.github.com>
2022-01-02 21:15:49 +0800

    Update

---
+++ syb.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+    Star Yuuki Bot
+    ~~~~~~~~~~~
+    Version:0.0001
+    Copyright: (c) 2015 SuperSonic Software Foundation and  Star Inc.
+    Website:
+        SuperSonic Software Foundation: http://supersonic-org.cf
+        Star Inc.: http://startw.cf
+        Star Neptune Bot: http://snb.startw.cf
+    License: Mozilla Public License 2.0
+"""
+
+print "Come Soon..."
